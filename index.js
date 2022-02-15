1ecbe5a7ec6e7676b109fbfc719cc74147477992
Cristina Oechsle <crisoechsle@gmail.com>
2022-02-15 09:58:47 +0100

    dynamic cards in js

--- index.js
+++ index.js
@@ -5,18 +5,30 @@ const description = [
   "Platform to rent a tent. Airbnb clone created with Ruby on Rails. ",
 ];
 
-const dynamic = document.querySelector(".container");
+// for (let i = 0; i < title.length; i++) {
+//   const fetch = document.querySelector(".container").innerHTML;
+//   dynamic.innerHTML =
+//     `<div id="cards${i}" class="boxes">
+//           <div class="box-content">
+//             <h4>${title[i]}</h4>
+//             <p>${description[i]}</p>
+//             <a href="#">more</a>
+//           </div>
+//         </div>` + fetch;
+//   const bgimg = document.getElementById(`cards${i}`);
+//   bgimg.style.backgroundImage = `url("images/${title[i]}.png")`;
+// }
+
+const cards = document.querySelector(".cards");
 
 for (let i = 0; i < title.length; i++) {
-  const fetch = document.querySelector(".container").innerHTML;
-  dynamic.innerHTML =
-    `<div id="cards${i}" class="boxes">
-          <div class="box-content">
-            <h4>${title[i]}</h4>
-            <p>${description[i]}</p>
-            <a href="#">more</a>
-          </div>
-        </div>` + fetch;
-  const bgimg = document.getElementById(`cards${i}`);
-  bgimg.style.backgroundImage = `url("images/${title[i]}.png")`;
+  const cardContent = cards.innerHTML;
+  cards.innerHTML =
+    ` <div class="card">
+        <img src="images/${title[i]}.png" alt="Avatar" style="width:100%">
+        <div class="container">
+          <h4>${title[i]}</h4>
+          <p>${description[i]}</p>
+        </div>
+      </div>` + cardContent;
 }
